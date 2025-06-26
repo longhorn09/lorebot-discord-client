6ec158fd77012b83412fbb17da733356dbb4615d
Norman Tang <norman@normstorm.com>
2025-06-26 00:51:32 -0400

    lore parsing of chat paste and graphql construction stared, direct message not working

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
   ],
 });
 
